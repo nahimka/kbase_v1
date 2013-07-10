3c2a6ad8dfd912f1a7cc9bc29b75d6b02061873e
fima.baranov <nahimkaorama@gmail.com>
2013-07-10 18:46:12 +0700

    Validation!

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 #raise 'foo'
 class PostTest < ActiveSupport::TestCase
+  
   test "post attributes must not be empty" do 
-    post = Post.new
+    post = Post.new(title:  posts(:dss_hive).title,
+        body: "sdfsd",
+        user_id: "fm",
+        version: posts(:dss_hive).version)
     assert post.invalid?
-    assert post.errors[:title].any?
-    assert post.errors[:body].any?
-    assert post.errors[:user_id].any?
-    assert post.errors[:version].any?
-    puts %{this is the title}
-    puts post.title
-    post.title = %{first title}
-    puts %{this is the title}
-    puts post.title
-  end  
+    # assert post.invalid?
+    # assert post.errors[:title].any?
+    # assert post.errors[:body].any?
+    # assert post.errors[:user_id].any?
+    # assert post.errors[:version].any?    
+  end
 end
